0941c5cbe94f6a03c1917afe86e31072a6df1df2
Mohammad <m.darestani98@gmail.com>
2024-11-12 14:08:24 +0800

    [add] database interface

---
+++ src/database/base_database.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class DatabaseAPI(ABC):
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+
+    @abstractmethod
+    def search(self, query: str, count: int):
+        pass
+
+    @abstractmethod
+    def parse(self, data):
+        pass
+
+    @abstractmethod
+    def generate_query(self, query: str):
+        pass
